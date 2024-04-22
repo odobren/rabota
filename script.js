3723d20f7aa48e54dcc07e6cb46b2e9b3e86a952
odobren <167509358+odobren@users.noreply.github.com>
2024-04-22 22:50:25 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+$(document).ready(function() {
+  // Разрешенный IP-адрес
+  var allowedIP = '178.129.244.145';
 
+  // Функция для получения IP-адреса пользователя
+  function getUserIP(callback) {
+    $.getJSON('https://api.ipify.org?format=json', function(data) {
+      callback(data.ip);
+    });
+  }
+
+  // Проверяем IP-адрес пользователя и перенаправляем, если не разрешен
+  getUserIP(function(userIP) {
+    if (userIP !== allowedIP) {
+      window.location.href = 'https://test13423424.glitch.me/zapret.html'; // Замените на вашу страницу отказа в доступе
+    }
+  });
 
   $('#employeeForm').submit(function(e) {
     e.preventDefault();
