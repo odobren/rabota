cba6dcfa4a8e66c6c035f21c24334e417ea24a7f
odobren <167509358+odobren@users.noreply.github.com>
2024-04-23 12:51:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-
 document.addEventListener('DOMContentLoaded', function() {
+
+  var employeeForm = document.getElementById('employeeForm');
+  var startDayBtn = document.getElementById('startDayBtn');
+  var endDayBtn = document.getElementById('endDayBtn');
+  var loader = document.getElementById('loader');
 
   // Загружаем данные из локального хранилища, если они есть
   var savedName = localStorage.getItem('employeeName');
@@ -7,19 +11,19 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('name').value = savedName;
   }
 
-  document.getElementById('employeeForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    var formData = new FormData(this);
+  function submitForm(endpointUrl) {
+    var formData = new FormData(employeeForm);
     var employeeName = formData.get('name');
 
     // Сохраняем имя в локальном хранилище
     localStorage.setItem('employeeName', employeeName);
 
     // Показываем анимацию загрузки
-    document.getElementById('startDayBtn').style.display = 'none';
-    document.getElementById('loader').style.display = 'block';
+    startDayBtn.disabled = true;
+    endDayBtn.disabled = true;
+    loader.style.display = 'block';
 
-    fetch('https://script.google.com/macros/s/AKfycbwqx3CH75w1dG1KWif0b9EnrIwKb8Mwlb_GbVn5rnshofcAF-WqxKVcgBJ9haGgbkOE/exec', {
+    fetch(endpointUrl, {
         method: 'POST',
         body: formData
     })
@@ -37,9 +41,21 @@ document.addEventListener('DOMContentLoaded', function() {
       console.error('Произошла ошибка:', error);
     })
     .finally(() => {
-      // По окончании запроса скрываем анимацию загрузки и показываем кнопку
-      document.getElementById('loader').style.display = 'none';
-      document.getElementById('startDayBtn').style.display = 'block';
+      // По окончании запроса скрываем анимацию загрузки и включаем кнопки
+      loader.style.display = 'none';
+      startDayBtn.disabled = false;
+      endDayBtn.disabled = false;
     });
+  }
+
+  // Обработчик события для кнопки "Начать день"
+  startDayBtn.addEventListener('click', function() {
+    submitForm('https://script.google.com/macros/s/AKfycbwqx3CH75w1dG1KWif0b9EnrIwKb8Mwlb_GbVn5rnshofcAF-WqxKVcgBJ9haGgbkOE/exec');
   });
+
+  // Обработчик события для кнопки "Завершить день"
+  endDayBtn.addEventListener('click', function() {
+    submitForm('https://script.google.com/macros/s/AKfycbwqx3CH75w1dG1KWif0b9EnrIwKb8Mwlb_GbVn5rnshofcAF-WqxKVcgBJ9haGgbkOE/exec');
+  });
+
 });
