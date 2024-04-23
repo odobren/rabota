a98b90df466320727f8ee67ea50a86e2ea785d4a
odobren <167509358+odobren@users.noreply.github.com>
2024-04-23 12:54:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  var employeeForm = document.getElementById('employeeForm');
-  var startDayBtn = document.getElementById('startDayBtn');
-  var endDayBtn = document.getElementById('endDayBtn');
-  var loader = document.getElementById('loader');
-
   // Загружаем данные из локального хранилища, если они есть
   var savedName = localStorage.getItem('employeeName');
   if (savedName) {
     document.getElementById('name').value = savedName;
   }
 
-  function submitForm(endpointUrl) {
-    var formData = new FormData(employeeForm);
+  document.getElementById('employeeForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
     var employeeName = formData.get('name');
 
     // Сохраняем имя в локальном хранилище
     localStorage.setItem('employeeName', employeeName);
 
     // Показываем анимацию загрузки
-    startDayBtn.disabled = true;
-    endDayBtn.disabled = true;
-    loader.style.display = 'block';
+    document.getElementById('startDayBtn').style.display = 'none';
+    document.getElementById('endDayBtn').style.display = 'none';
+    document.getElementById('loader').style.display = 'block';
 
-    fetch(endpointUrl, {
+    fetch('https://script.google.com/macros/s/AKfycbwqx3CH75w1dG1KWif0b9EnrIwKb8Mwlb_GbVn5rnshofcAF-WqxKVcgBJ9haGgbkOE/exec', {
         method: 'POST',
         body: formData
     })
@@ -34,28 +30,27 @@ document.addEventListener('DOMContentLoaded', function() {
       return response.text();
     })
     .then(data => {
-      // Перенаправляем пользователя на указанную страницу
-      window.location.href = 'https://odobren.github.io/rabota/ura';
+      // Перенаправляем пользователя на указанную страницу в зависимости от нажатой кнопки
+      if (document.getElementById('startDayBtn').style.display === 'none') {
+        window.location.href = 'https://odobren.github.io/rabota/ura';
+      } else {
+        window.location.href = 'https://odobren.github.io/rabota/poka';
+      }
     })
     .catch(error => {
       console.error('Произошла ошибка:', error);
     })
     .finally(() => {
-      // По окончании запроса скрываем анимацию загрузки и включаем кнопки
-      loader.style.display = 'none';
-      startDayBtn.disabled = false;
-      endDayBtn.disabled = false;
+      // По окончании запроса скрываем анимацию загрузки и показываем кнопку
+      document.getElementById('loader').style.display = 'none';
+      document.getElementById('startDayBtn').style.display = 'block';
+      document.getElementById('endDayBtn').style.display = 'block';
     });
-  }
-
-  // Обработчик события для кнопки "Начать день"
-  startDayBtn.addEventListener('click', function() {
-    submitForm('https://script.google.com/macros/s/AKfycbwqx3CH75w1dG1KWif0b9EnrIwKb8Mwlb_GbVn5rnshofcAF-WqxKVcgBJ9haGgbkOE/exec');
   });
 
-  // Обработчик события для кнопки "Завершить день"
-  endDayBtn.addEventListener('click', function() {
-    submitForm('https://script.google.com/macros/s/AKfycbwqx3CH75w1dG1KWif0b9EnrIwKb8Mwlb_GbVn5rnshofcAF-WqxKVcgBJ9haGgbkOE/exec');
+  // Добавляем обработчик для кнопки "Завершить день"
+  document.getElementById('endDayBtn').addEventListener('click', function() {
+    // При клике на кнопку "Завершить день" происходит отправка формы с пустыми данными
+    document.getElementById('employeeForm').submit();
   });
-
 });
